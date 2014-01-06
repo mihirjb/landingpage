554e85442df6124413bdc853c6e5db3e5eb2c3c0
Mihir <mihirjb@gmail.com>
2014-01-06 16:57:19 +0800

    able to signup now

--- app/models/evangelist.rb
+++ app/models/evangelist.rb
@@ -14,7 +14,5 @@ class Evangelist < ActiveRecord::Base
     validates_presence_of :email
    validates_uniqueness_of :email, case_sensitive: false
    validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-   validates_presence_of :fullname
-   validates_presence_of :phone
-   
+   validates_presence_of :fullname   
 end
